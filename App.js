959a4bc80ebae3fe9b88ebb446e3421bdcebe0cb
Lane Katris <lanekatris@gmail.com>
2021-01-30 19:49:53 -0500

    console error removed

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
 import Providers from './navigation';
+import { LogBox } from 'react-native';
+import clone from 'lodash/clone';
+
+LogBox.ignoreLogs(['Setting a timer']);
+const _console = clone(console);
+console.warn = message => {
+  if (message.indexOf('Setting a timer') <= -1) {
+    _console.warn(message);
+  }
+};
 
 export default function App() {
   return <Providers />;
