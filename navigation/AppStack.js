82a0edccf42993d12c630eb91e25caae2f8d9901
Lane Katris <lanekatris@gmail.com>
2021-02-03 08:15:49 -0500

    added logout button

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import HomeScreen from '../screens/HomeScreen';
 import {Button, Icon} from 'react-native-elements';
@@ -9,29 +9,76 @@ import createClimb from "../commands/create-climb";
 import ClimbScreen from "../screens/ClimbScreen";
 import {DETAIL_VIEW_SCREEN} from "../utils/colors";
 // import {useNavigation} from "@react-navigation/native";
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import SettingsScreen from "../screens/SettingsScreen";
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
-export default function AppStack() {
-  const {user:{uid}} = useContext(AuthUserContext);
+function AppStack() {
+  const {user: {uid}} = useContext(AuthUserContext);
   // const navigation = useNavigation();
-  console.log('uid',uid)
+  console.log('uid', uid)
 
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Your Climbs" component={HomeScreen}
+    <>
+      <Stack.Navigator>
 
-      options={({navigation}) => ({
-        headerRight: () => <Button  type="clear" icon={<Icon name="add" type="material" onPress={async () => {
-        const newClimb = await createClimb(uid)
-          console.log('newclimb', newClimb)
-        navigation.navigate(DETAIL_VIEW_SCREEN ,newClimb)
-      }} />} />
-      })} />
+        <Stack.Screen name="Your Climbs" component={HomeScreen}
 
-      <Stack.Screen name={DETAIL_VIEW_SCREEN} component={ClimbScreen} options={({route}) => ({
-        title: `${route.params.name}`
-      })} />
-    </Stack.Navigator>
+                      options={({navigation}) => ({
+                        headerRight: () => <Button type="clear"
+                                                   icon={<Icon name="add" type="material" onPress={async () => {
+                                                     const newClimb = await createClimb(uid)
+                                                     console.log('newclimb', newClimb)
+                                                     navigation.navigate(DETAIL_VIEW_SCREEN, newClimb)
+                                                   }}/>}/>,
+                      })}/>
+
+        <Stack.Screen name={DETAIL_VIEW_SCREEN} component={ClimbScreen} options={({route}) => ({
+          title: `${route.params.name}`
+        })}/>
+      </Stack.Navigator>
+    </>
   );
+}
+
+const Tab = createBottomTabNavigator();
+
+
+function SettingsStack() {
+  return <Stack.Navigator>
+    <Stack.Screen name="Settings" component={SettingsScreen} />
+  </Stack.Navigator>
+}
+
+export default function BottomTabNavigator(){
+  return <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === 'Home') {
+          // iconName = focused
+          //   ? 'ios-information-circle'
+          //   : 'ios-information-circle-outline';
+          iconName = 'home'
+        }
+        else if (route.name === 'Settings') {
+          iconName = 'settings'
+        }
+
+        // You can return any component that you like here!
+        return <Icon name={iconName} size={size} color={color}/>;
+      },
+    })}
+    tabBarOptions={{
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    }}
+  >
+    <Tab.Screen name="Home" component={AppStack} />
+    <Tab.Screen name="Settings" component={SettingsStack} />
+  </Tab.Navigator>
 }
---
+++ screens/SettingsScreen.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import {ScrollView} from "react-native";
+import {Button, Header, ListItem, Text, SocialIcon, ThemeProvider, Divider, Icon} from 'react-native-elements';
+import {logout} from "../components/Firebase/firebase";
+
+export default function SettingsScreen(){
+  return <ScrollView>
+    <ListItem onPress={logout}>
+<Icon name="logout" type="material" />
+
+    <ListItem.Content>
+      <ListItem.Title>Log Out</ListItem.Title>
+    </ListItem.Content>
+    </ListItem>
+  </ScrollView>
+}
