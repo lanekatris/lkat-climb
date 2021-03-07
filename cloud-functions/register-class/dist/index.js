d8ca47fa98f510973a1e403ae42bf2975b18989b
Lane Katris <lane.katris@community.co>
2021-03-06 22:07:25 -0500

    keeping dist just so its easy to work with cloud functions

---
+++ cloud-functions/register-class/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.helloGET = function (req, res) {
+    res.send('Hello World!');
+};
+//# sourceMappingURL=index.js.map
